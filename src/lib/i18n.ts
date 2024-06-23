081369cc341a7ddf4bca8c6126372c92c50a6f41
AwesomePGP <awesomepgp@proton.me>
2024-06-23 21:46:09 +0200

    Actually load locales

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,20 +2,36 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import resourcesToBackend from "i18next-resources-to-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import translationsEn from "@/locales/en/common.json";
+import translationsFr from "@/locales/fr/common.json";
+
 export const supportedLanguages = [
   { code: "en", label: "English" },
   { code: "fr", label: "Français" },
 ];
+const translations = {
+  common: {
+    en: translationsEn,
+    fr: translationsFr,
+  },
+};
 
 // eslint-disable-next-line import/no-named-as-default-member
 i18next
   // load translation using neutralino. See locales folder
   // learn more: https://github.com/i18next/i18next-resources-to-backend
   .use(
-    resourcesToBackend(
-      (language: string, namespace: string) =>
-        import(`../locales/${language}/${namespace}.json`)
-    )
+    resourcesToBackend((language: string, namespace: string) => {
+      if (!(namespace in translations)) {
+        return {};
+      }
+      const ns = namespace as keyof typeof translations;
+      if (!(language in translations[ns])) {
+        return {};
+      }
+      const lang = language as keyof (typeof translations)[typeof ns];
+      return translations[ns][lang];
+    })
   )
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
