9a395c8d8e1b11b7b8e40d7b2561c4b00625e0b2
AwesomePGP <awesomepgp@proton.me>
2024-07-07 23:24:28 +0200

    main compilation knows about ts-paths

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,5 +1,6 @@
 import type { ConfigEnv, UserConfig } from "vite";
 import { defineConfig, mergeConfig } from "vite";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 import {
   getBuildConfig,
   getBuildDefine,
@@ -23,7 +24,7 @@ export default defineConfig((env) => {
         external,
       },
     },
-    plugins: [pluginHotRestart("restart")],
+    plugins: [pluginHotRestart("restart"), viteTsconfigPaths()],
     define,
     resolve: {
       // Load the Node.js entry.
